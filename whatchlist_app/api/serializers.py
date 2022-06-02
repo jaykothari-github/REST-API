b96ba6328a8512c5e8f547a8eeeca2e1c98a4b9d
JayKothari <jaykothari49@gmail.com>
2022-06-02 11:40:29 +0530

    Model Serializer added

--- whatchlist_app/api/serializers.py
+++ whatchlist_app/api/serializers.py
@@ -1,36 +1,60 @@
-from wsgiref.validate import validator
-from django.forms import ValidationError
 from rest_framework import serializers
 from whatchlist_app.models import Movie
 
-def check_name(value):
-    if value == 'Jay':
-        raise ValidationError('Not Jay')
-    return value
+# --------------------------------------serializers.ModelSerializer class--------------------------------- #
 
-class MovieSerializer(serializers.Serializer):
+class MovieSerializer(serializers.ModelSerializer):
 
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[check_name])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
-    def create(self,validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self,instance,validated_data):
-        instance.name = validated_data.get('name',instance.name)
-        instance.description = validated_data.get('description',instance.description)
-        instance.active = validated_data.get('active',instance.active)
-        instance.save()
-        return instance
-
+    
+#   Field Level Validation  (validate_field)
     def validate_name(self, value):
         if len(value) <= 1:
             raise serializers.ValidationError("Name lenght should be gt 1")
         return value
 
+#   Object Level Validation
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError("Name and Des should be different...")
         return data
+
+
+# --------------------------------------serializers.Serializer class---------------------------------  #
+#   Validators Functions
+# def check_name(value):
+#     if value == 'Jay':
+#         raise ValidationError('Not Jay')
+#     return value
+
+# class MovieSerializer(serializers.Serializer):
+
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[check_name])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     def create(self,validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     def update(self,instance,validated_data):
+#         instance.name = validated_data.get('name',instance.name)
+#         instance.description = validated_data.get('description',instance.description)
+#         instance.active = validated_data.get('active',instance.active)
+#         instance.save()
+#         return instance
+
+# #   Field Level Validation  (validate_field)
+#     def validate_name(self, value):
+#         if len(value) <= 1:
+#             raise serializers.ValidationError("Name lenght should be gt 1")
+#         return value
+
+# #   Object Level Validation
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("Name and Des should be different...")
+#         return data
